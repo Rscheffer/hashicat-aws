1086728fd9cea0ca6db2793e945b85b0142e9503
rscheffer <rscheffer@presidio.com>
2021-04-21 20:33:54 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/Randall-training/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  bucket_prefix = var.prefix 
-  
+  bucket_prefix = "randallscheffer"
+
 }
